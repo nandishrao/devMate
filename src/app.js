d9d80e2ecc8a239169e07d19f6b0f42acdb37077
nandishrao <nandishhraoo@gmail.com>
2025-09-25 17:01:32 +0530

    Express Routes - Dynamic Routing and Query Params

--- src/app.js
+++ src/app.js
@@ -3,15 +3,19 @@ const app = express();
 app.listen(3000, ()=>{
     console.log("The Server is Running on port 3000")
 })
-app.use("/nandish" ,(req,res)=>{
-    res.send("this is from the nandish's path")
+app.get("/user/:userId/:Name/:Password",(req,res)=>{
+    console.log(req.params)
+    res.send({firstName : "Nandish",    lastName : "Rao"})
 })
+ 
+app.post("/user" , (req,res)=>{
+    res.send("The user data has been updated in the database")
+})
+
 app.use("/test", (req,res)=>{
     res.send("from test path")
 })
-app.use("/", (req,res)=>{
-    res.send("from slash path")
-})
+
 
 
 
--- src/constants.js
+++
@@ -1,4 +0,0 @@
-//this is the constants file
-// this is too also check the ctrl+shift+g
-//trying git lens
-const a =10
