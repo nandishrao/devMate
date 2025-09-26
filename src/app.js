b6354675992f2127539d87d673267f7c85062389
nandishrao <nandishhraoo@gmail.com>
2025-09-26 23:07:09 +0530

    sending dymanic data from postman to the database
    Signed-off-by: nandishrao <nandishhraoo@gmail.com>

--- src/app.js
+++ src/app.js
@@ -2,14 +2,13 @@ const express= require("express")
 const connectDB =require("./config/database")
 const app = express();
 const User = require ("./models/user")
+app.use(express.json())
 
-app.post("/signup" , async ( req , res )=>{
-    const user = new User({
-        firstName: "Nandish",
-        lastName : "Rao",
-        password : "NandishRao@123",
-        email: "nandishhraoo@gmail.com",
-    })
+
+
+app.post("/signup" , async ( req , res )=>{ 
+    //creating a new instance of the user model
+     const user = new User(req.body)
      try{
         await user.save()
      res.send("user data saved Succesfully ")
