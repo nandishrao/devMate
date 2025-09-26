6a6db042b324d92512bd38d7da1457e1cc8e3df8
nandishrao <nandishhraoo@gmail.com>
2025-09-26 10:32:07 +0530

    writing auth middleware
    Signed-off-by: nandishrao <nandishhraoo@gmail.com>

--- src/app.js
+++ src/app.js
@@ -3,29 +3,16 @@ const app = express();
 app.listen(3000, ()=>{
     console.log("The Server is Running on port 3000")
 })
-
-app.use("/user", 
-    (req,res,next)=>{
-    next()
-},
-(req,res,next)=>{
-    console.log("this is from the second handler")
-    next()
-},
-(req , res, next)=>{
-    console.log("third handler")
-    next();
-},
-(req , res, next)=>{
-    console.log("fourth handler")
-    next();
-},
-(req , res, next)=>{
-    console.log("fifth handler")
-    res.send("from the fifth handler")
-}
-)
-
+app.use("/" , (req, res)=>{
+    const token="abc"
+    const Authorised_admin = token === "abc"
+    if(!Authorised_admin){
+        res.status(401).send("you are not authorised")
+    }
+    else{
+        
+    }
+})
 
 
 
