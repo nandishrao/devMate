6bf38daabff33b8cbef864fc4b02bafeddfd7a6a
nandishrao <nandishhraoo@gmail.com>
2025-09-26 19:45:29 +0530

    created a UserSchema and made  a POST API call and added the data to the database
    Signed-off-by: nandishrao <nandishhraoo@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 const express= require("express")
 const connectDB =require("./config/database")
 const app = express();
+const User = require ("./models/user")
+
+app.post("/signup" , async ( req , res )=>{
+    const user = new User({
+        firstName: "Nandish",
+        lastName : "Rao",
+        password : "NandishRao@123",
+        email: "nandishhraoo@gmail.com",
+    })
+     try{
+        await user.save()
+     res.send("user data saved Succesfully ")
+     }catch(err){
+        res.status(400).send("Error saving the data"  + err.message)
+     }
+})
+
+ 
+
+
+
 
 connectDB().then(()=>{
     console.log("Database connection is Successfull")
