58d1a0a94ca12d702f9fcc1be0e0329bbbd23052
nandishrao <nandishhraoo@gmail.com>
2025-09-27 22:58:17 +0530

    Added Schema Validations
    Signed-off-by: nandishrao <nandishhraoo@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,77 +1,76 @@
-const express= require("express")
-const connectDB =require("./config/database")
+const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
-const User = require ("./models/user")
+const User = require("./models/user");
 
-app.use(express.json())//middleware helps in converting json 
+app.use(express.json()); //middleware helps in converting json
 //save an user in the database
-app.post("/signup" , async ( req , res )=>{ 
-    //creating a new instance of the user model
-     const user = new User(req.body)
-     try{
-        await user.save()
-     res.send("user data saved Succesfully ")
-     }catch(err){
-        res.status(400).send("Error saving the data"  + err.message)
-     }
-})
+app.post("/signup", async (req, res) => {
+  //creating a new instance of the user model
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("user data saved Succesfully ");
+  } catch (err) {
+    res.status(400).send("Error saving the data" + err.message);
+  }
+});
 
 //GET method to get user by a particular emailID
-app.get("/user" , async (req,res)=>{
-    const email = req.body.emailId  
+app.get("/user", async (req, res) => {
+  const email = req.body.emailId;
 
-    try{
-    const user = await User.find({emailId : email})
-    if(user.length === 0){
-        res.send("user not found")
-    }else{
-        res.send(user)
+  try {
+    const user = await User.find({ emailId: email });
+    if (user.length === 0) {
+      res.send("user not found");
+    } else {
+      res.send(user);
     }
-    }catch (err){
-    res.status(400).send("Couldn't fetch user")
-    }
-})
+  } catch (err) {
+    res.status(400).send("Couldn't fetch user");
+  }
+});
 
 //GET method to get all the users to show on the feed
-app.get("/feed" , async (req,res)=>{
-   try{
-     const users = await User.find({})
-     res.send(users)
-   }catch(err){
-    res.status(500).send("something went wrong" )
-   }
-})
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send("something went wrong");
+  }
+});
 
- //DELETE method to delete a user by MONGOID
- app.delete("/user" ,  async (req , res)=>{
-   try{
-    const id = req.body.UserID
-    const user = await User.findByIdAndDelete(id)
-    res.send("the user is successfully deleted ")
-   }catch(err){
-    res.status(500).send("There was some problem deleting the user")
-   }
-
- })
+//DELETE method to delete a user by MONGOID
+app.delete("/user", async (req, res) => {
+  try {
+    const id = req.body.UserID;
+    const user = await User.findByIdAndDelete(id);
+    res.send("the user is successfully deleted ");
+  } catch (err) {
+    res.status(500).send("There was some problem deleting the user");
+  }
+});
 //PATCH method to update user data
- app.patch("/user" , async( req , res )=>{
-    const userID = req.body.UserID
-    const data = req.body
-    try{
-        await User.findByIdAndUpdate(userID , data)
-        res.send("the user details was updated")
-    }catch(err){
-        res.send("the update was failed")
-    }
- })
+app.patch("/user", async (req, res) => {
+  const userID = req.body.UserID;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate(userID, data ,{runValidators : true} );
+    res.send("the user details was updated");
+  } catch (err) {
+    res.send("the update was failed");
+  }
+});
 
-connectDB().then(()=>{
-    console.log("Database connection is Successfull")
-    app.listen(3000, ()=>{
-    console.log("The Server is Running on port 3000")
-})
-}).catch(err=>{
-    console.log("database connection failed")
-})
-
- 
+connectDB()
+  .then(() => {
+    console.log("Database connection is Successfull");
+    app.listen(3000, () => {
+      console.log("The Server is Running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("database connection failed");
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,52 @@ const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
     firstName : {
-        type : String
+        type : String,
+        required : true,
+        unique : true,
+        trim : true,
+        maxLength : 50,
+        minLength:5
     },
     lastName : {
-        type : String 
+        type : String,
+        trim : true,
     },
     password: {
-        type : String 
+        type : String,
+        required : true 
     },
     emailId : {
-        type : String
+        type:String,
+        required: true,
+        unique: true,
+        lowercase : true,
+        trim : true,
     },
     age: {
-        type : Number
+        type : Number,
+        min : 18
     },
     gender : {
-        type : String
+        type : String,
+        validate(value){
+            if(!["male" , "female" , "others"].includes(value)){
+                throw new Error("The Gender is not valid")
+            }
+        }
+    },
+    about : {
+        type : String,
+        default : "hi i am using devTinder",
+        trim : true,
+    },
+    photoURL :{
+        type : String,
+        default : "./Dummy.png"
+    },
+    skills : {
+        type : [String]
     }
-})
+} , {timestamps : true})
  
 module.exports = mongoose.model("User" , userSchema)
