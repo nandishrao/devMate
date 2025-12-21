3b9397ae3ffaa6fd11ff3104b64b29cf1da18c35
Nandish Rao <nandishhraoo@gmail.com>
2025-12-21 21:25:31 +0530

    fix  : updated the chat feaature by hashing and securing roomId
    Signed-off-by: Nandish Rao <nandishhraoo@gmail.com>

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,17 +1,52 @@
 const socket = require("socket.io");
+const Chat = require("../models/chat");
+const crypto = require("crypto");
 
+const getSecretRoomId = (userId, toTargetUser) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, toTargetUser].sort().join("$"))
+    .digest("hex");
+};
 const initializeSocket = (server) => {
   const io = socket(server, {
-    cors: { 
-        origin: "http://localhost:5173",
-        credentials: true,
+    cors: {
+      origin: "http://localhost:5173",
+      credentials: true,
     },
-  });   
-    io.on("connection", (socket) => {
-       //handle socket events here
+  });
+  io.on("connection", (socket) => {
+    socket.on("joinChat", ({ firstName, userId, toTargetUser }) => {
+      const roomId = getSecretRoomId(userId, toTargetUser);
+      socket.join(roomId);
     });
 
-    return io;
+    socket.on(
+      "sendMessage",
+      async ({ firstName: firstName, userId, toTargetUser, text }) => {
+        // save the messages in the database
+        try {
+          const room = getSecretRoomId(userId, toTargetUser);
+          let chat = await Chat.findOne({
+            participants: { $all: [userId, toTargetUser] },
+          });
+          if (!chat) {
+            chat = new Chat({
+              participants: [userId, toTargetUser],
+              messages: [],
+            });
+          }
+          chat.messages.push({ senderId: userId, text });
+          io.to(room).emit("receivedMessage", { firstName, text });
+          await chat.save();
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    );
+
+    socket.on("disconnect", () => {});
+  });
 };
 
 module.exports = initializeSocket;
